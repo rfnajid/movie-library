b5634a5b71548be3707e612680ed51f394e1f681
rida <ridhaaaaazis@gmail.com>
2023-02-05 18:56:33 +0700

    Add description to  schema

--- src/graphql/schemas/movie.ts
+++ src/graphql/schemas/movie.ts
@@ -2,27 +2,44 @@
 export const movieSchema = `#graphql
 
  type Movie {
-     id: Int!
-     title: String!
-     description: String
-     posterUrl: String
-     actors: [Person]
-     authors: [Person]
+   id: Int!
+   title: String!
+   description: String
+   posterUrl: String
+   actors: [Person]
+   authors: [Person]
  }
 
  extend type Query {
-    findAllMovie: [Movie]
-    findOneMovie(id: Int!): Movie
+   "Get all movies"
+   findAllMovie: [Movie]
+   
+   "Get one movie by id"
+   findOneMovie(id: Int!): Movie
  }
 
  extend type Mutation {
-    createMovie(title: String!, description: String, posterUrl: String): Movie @isAuthenticated
-    updateMovie(id: Int, title: String, description: String, posterUrl: String): Movie @isAuthenticated
-    deleteMovie(id: Int!): String @isAuthenticated
-    addActor(movieId: Int!, actorId: Int!): Movie @isAuthenticated
-    deleteActor(movieId: Int!, actorId: Int!): Movie @isAuthenticated
-    addAuthor(movieId: Int!, authorId: Int!): Movie @isAuthenticated
-    deleteAuthor(movieId: Int!, authorId: Int!): Movie @isAuthenticated
+   
+   "Insert New Movie"
+   createMovie(title: String!, description: String, posterUrl: String): Movie @isAuthenticated
+   
+   "Update existing movie"
+   updateMovie(id: Int, title: String, description: String, posterUrl: String): Movie @isAuthenticated
+   
+   "Delete movie by id"
+   deleteMovie(id: Int!): String @isAuthenticated
+   
+   "Add an actor to a movie"
+   addActor(movieId: Int!, actorId: Int!): Movie @isAuthenticated
+   
+   "Delete an actor from a movie"
+   deleteActor(movieId: Int!, actorId: Int!): Movie @isAuthenticated
+   
+   "Add an author to a movie"
+   addAuthor(movieId: Int!, authorId: Int!): Movie @isAuthenticated
+   
+   "Delete an author from a movie"
+   deleteAuthor(movieId: Int!, authorId: Int!): Movie @isAuthenticated
  }
 
 `;
--- src/graphql/schemas/person.ts
+++ src/graphql/schemas/person.ts
@@ -2,23 +2,33 @@
 export const personSchema = `#graphql
 
  type Person {
-     id: Int!
-     name: String!
-     bio: String
-     photoUrl: String
-     actorIn: [Movie]
-     authorIn: [Movie]
+   id: Int!
+   name: String!
+   bio: String
+   photoUrl: String
+   actorIn: [Movie]
+   authorIn: [Movie]
  }
 
- extend type Query { 
-    findAllPerson: [Person]
-    findOnePerson(id: Int!): Person
+ extend type Query {
+   
+   "Get all person"
+   findAllPerson: [Person]
+   
+   "get one person by id"
+   findOnePerson(id: Int!): Person
  }
 
  extend type Mutation {
-    createPerson(name: String!, bio: String, photoUrl: String): Person @isAuthenticated
-    updatePerson(id: Int, name: String, bio: String, photoUrl: String): Person @isAuthenticated
-    deletePerson(id: Int!): String @isAuthenticated
+   
+   "Create a new Person. Person can be author or actor"
+   createPerson(name: String!, bio: String, photoUrl: String): Person @isAuthenticated
+   
+   "Update existing person"
+   updatePerson(id: Int, name: String, bio: String, photoUrl: String): Person @isAuthenticated
+   
+   "Delete person"
+   deletePerson(id: Int!): String @isAuthenticated
  }
 
 `;
